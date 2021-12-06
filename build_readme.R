9236dec55d2f4b87a9cff02f136b4e4d05ebd413
Harvey Lieberman <harvey.lieberman@gmail.com>
2021-12-06 14:43:08 -0500

    build README

---
+++ build_readme.R
@@ -0,0 +1,59 @@
+#### This script builds the README.md
+#### for R/Pharma 2021 workshops and talks
+
+library(tidyverse)
+library(glue)
+
+## Params
+
+loc_data <- "talks_table.csv"
+
+## Process
+
+data <- read_csv(loc_data,  locale = locale(encoding = 'latin1'), na = character())
+
+## Print
+sink('README.md')
+
+cat("## Building this document\n\n")
+cat("To build this README, run `build_readme.R`. Talks data is in csv `talks_table.csv`\n\n")
+
+
+cat("## Workshops\n\n")
+data %>%
+  filter(Type == 'workshop') %>%
+  rowwise() %>%
+  mutate(txt = paste0(paste0("<strong>", unlist(strsplit(Name, " // ")), "</strong> (<i>", unlist(strsplit(Affiliation, " // ")), "</i>)"), collapse = ", ")) %>%
+  mutate(video_link = if_else(nchar(youtube) > 0, paste0("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;[Workshop Recording](", youtube, ")"), "")) %>%
+  glue_data(
+    "{txt}<br>{Title}",
+    "<details><summary>Abstract</summary>",
+    "<p>{Abstract}</p>",
+    "</details>",
+    "[Link to Workshop Material]({Slides})",
+    "{video_link}",
+    "<br><br>"
+  )
+
+x <- lapply(1:3, function(i) {
+  data %>%
+    filter(Type == "talk") %>%
+    filter(Day  == i) %>%
+    rowwise() %>%
+    mutate(txt = paste0(paste0("<strong>", unlist(strsplit(Name, " // ")), "</strong> (<i>", unlist(strsplit(Affiliation, " // ")), "</i>)"), collapse = ", ")) %>%
+    mutate(video_link = if_else(nchar(youtube) > 0, paste0("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;[Youtube](", youtube, ")"), "")) %>%
+    glue_data(
+      "{txt}<br>{Title}",
+      "<details><summary>Abstract</summary>",
+      "</p>{Abstract}</p>",
+      "</details>",
+      "[Slides]({Slides}){video_link}<br><br>"
+    )
+})
+
+for (i in 1:3) {
+  cat("\n\n## Talks - Day ", i, "\n\n")
+  cat(x[[i]])
+}
+
+sink()
